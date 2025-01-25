509838843a487591573ad28942eb07927b9b0ff3
Jack English <51383165+AUT0M4T1C-JACK@users.noreply.github.com>
2025-01-25 11:04:14 -0500

    Added wakeUpSound to floorDoor

--- Assets/Scripts/FloorDoorScript.cs
+++ Assets/Scripts/FloorDoorScript.cs
@@ -16,6 +16,7 @@ public class FloorDoorScript : MonoBehaviour
     public AudioSource leftDoorAudioSource;
     public AudioSource rightDoorAudioSource;
     public AudioSource floorAudioSource;
+    public AudioSource wakeUpSource;
   
     // Start is called before the first frame update
     void Start()
@@ -104,10 +105,15 @@ public class FloorDoorScript : MonoBehaviour
         {
             floorAudioSource.Stop();
         }
-        
+
         if (!leftDoorAudioSource.isPlaying)
         {
             leftDoorAudioSource.Play();
+        }
+
+        if (!wakeUpSource.isPlaying)
+        {
+            wakeUpSource.Play();
         }
     }
 }
