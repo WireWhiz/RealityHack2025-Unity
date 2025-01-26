beaab5e1136403bd0789a5d01f92586293a3bf6d
Jack English <51383165+AUT0M4T1C-JACK@users.noreply.github.com>
2025-01-26 09:23:07 -0500

    Added ship back to Gameplay scene and am working on the title and credits

---
+++ Assets/Scripts/TitleAndCreditController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAndCreditController : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject credits;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartAscent()
+    {
+
+    }
+
+    public void showCredits()
+    {
+
+    }
+}
